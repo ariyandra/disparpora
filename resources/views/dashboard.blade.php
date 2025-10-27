ff0ce475d403b492dd7de0cd7e4d945b136f312d
ariyandra <ariyandra35@gmail.com>
2025-10-27 09:02:49 +0700

    Initial commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -51,7 +51,6 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                                     </svg>
                                 </div>
-                                <span class="text-sm font-medium text-green-600 bg-green-100 px-3 py-1 rounded-full">Hadir</span>
                             </div>
                             <div class="text-center">
                                 <div class="text-5xl font-bold text-gray-800 mb-2 number-animation">{{ $jml_atlit ?? 0 }}</div>
@@ -70,7 +69,6 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                                     </svg>
                                 </div>
-                                <span class="text-sm font-medium text-blue-600 bg-blue-100 px-3 py-1 rounded-full">Tersedia</span>
                             </div>
                             <div class="text-center">
                                 <div class="text-5xl font-bold text-gray-800 mb-2 number-animation">{{ $jml_cabor ?? 0 }}</div>
@@ -89,7 +87,6 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
                                     </svg>
                                 </div>
-                                <span class="text-sm font-medium text-orange-600 bg-orange-100 px-3 py-1 rounded-full">Hadir</span>
                             </div>
                             <div class="text-center">
                                 <div class="text-5xl font-bold text-gray-800 mb-2 number-animation">{{ $jml_pelatih ?? 0 }}</div>
@@ -108,7 +105,6 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
                                     </svg>
                                 </div>
-                                <span class="text-sm font-medium text-green-600 bg-green-100 px-3 py-1 rounded-full">Tersedia</span>
                             </div>
                             <div class="text-center">
                                 <div class="text-5xl font-bold text-gray-800 mb-2 number-animation">{{ $jml_lapangan ?? 0 }}</div>
