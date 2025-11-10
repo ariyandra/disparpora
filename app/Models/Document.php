f27918e5cf5d393c59568d1f7d283905251232ea
ariyandra <ariyandra35@gmail.com>
2025-11-10 14:17:47 +0700

    Fix: re-encode Document.php to UTF-8

---
+++ app/Models/Document.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Document extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function documentable()
+    {
+        return $this->morphTo();
+    }
+}
+
